2deed64b819cbbee4514b9e2ca8ece53e2637d7d
simsevenx <cooperation@qq.com>
2020-06-09 19:37:00 +0800

    remove IdGenerator.java

--- x7-repo/x7-jdbc-template-plus/src/main/java/io/xream/x7/repository/internal/DefaultRepository.java
+++ x7-repo/x7-jdbc-template-plus/src/main/java/io/xream/x7/repository/internal/DefaultRepository.java
@@ -81,13 +81,9 @@ public abstract class DefaultRepository<T> implements BaseRepository<T> {
         this.clz = (Class) params[0];
 
         hook();
-
     }
 
     protected void hook() {
-        if (!EntityHolder.listAll().contains(this.clz)) {
-            EntityHolder.listAll().add(this.clz);
-        }
         if (!HealthChecker.getRepositoryList().contains(this)) {
             HealthChecker.getRepositoryList().add(this);
         }
